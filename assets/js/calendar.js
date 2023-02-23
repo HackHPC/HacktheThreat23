adb765897f95ee77e221f7b2140d6bef13891885
Jeaime Powell <jeaimehp@gmail.com>
2023-02-22 22:25:11 -0600

    fixed links

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -36,70 +36,70 @@ document.addEventListener('DOMContentLoaded', function() {
           start: '2023-02-28T11:00:00-06:00',
           end: '2023-02-28T12:00:00-06:00',
           backgroundColor: 'orange',
-          url: './resources/#Mentor%20Training',
+          url: './resources#Mentor%20Training',
         },
         {
           title: 'All Participants Orientation',
           start: '2023-02-23T11:00:00-06:00',
           end: '2023-02-23T12:00:00-06:00',
           backgroundColor: 'orange',
-          url: './resources/#All%20Participants%20Orientation'
+          url: './resources#All%20Participants%20Orientation'
         },
         {
           title: 'Hackathon Kick-Off!',
           start: '2023-03-02T18:00:00-06:00',
           end: '2023-03-02T20:00:00-06:00',
           backgroundColor: 'green',
-          url: './resources/#Hackathon%20Kick-Off!'
+          url: './resources#Hackathon%20Kick-Off!'
         },
         {
           title: 'Friday Morning Checkin',
           start: '2023-03-03T11:00:00-06:00',
           end: '2023-03-03T12:00:00-06:00',
           //backgroundColor: 'orange'
-          url: './resources/#Friday%20Morning%20Checkin'
+          url: './resources#Friday%20Morning%20Checkin'
         },
         {
           title: 'Friday Afternoon Checkin',
           start: '2023-03-03T18:00:00-06:00',
           end: '2023-03-03T19:00:00-06:00',
           //backgroundColor: 'orange'
-          url: './resources/#Friday%20Afternoon%20Checkin'
+          url: './resources#Friday%20Afternoon%20Checkin'
         },
         {
           title: 'Saturday Morning Checkin',
           start: '2023-03-04T11:00:00-06:00',
           end: '2023-03-04T12:00:00-06:00',
           //backgroundColor: 'orange'
-          url: './resources/#Saturday%20Morning%20Checkin'
+          url: './resources#Saturday%20Morning%20Checkin'
         },
         {
           title: 'Saturday Afternoon Checkin',
           start: '2023-03-04T18:00:00-06:00',
           end: '2023-03-04T19:00:00-06:00',
           //backgroundColor: 'orange'
-          url: './resources/#Saturday%20Afternoon%20Checkin'
+          url: './resources#Saturday%20Afternoon%20Checkin'
         },
         {
           title: 'Sunday Morning Checkin',
           start: '2023-03-05T11:00:00-06:00',
           end: '2023-03-05T12:00:00-06:00',
           //backgroundColor: 'orange'
-          url: './resources/#Sunday%20Morning%20Checkin'
+          url: './resources#Sunday%20Morning%20Checkin'
         },
         {
           title: 'Monday Morning Checkin',
           start: '2023-03-06T11:00:00-06:00',
           end: '2023-03-06T12:00:00-06:00',
           //backgroundColor: 'orange'
-          url: './resources/#Monday%20Morning%20Checkin'
+          url: './resources#Monday%20Morning%20Checkin'
         },
         {
           title: 'Final Presentations',
           start: '2023-03-06T18:00:00-06:00',
           end: '2023-03-06T20:00:00-06:00',
           backgroundColor: 'red',
-          url: './resources/#Final%20Presentations'
+          url: './resources#Final%20Presentations'
         },
       ]
     });
