425f51c18b905edd42d2d2ce79948736452ae56a
Jeaime Powell <jeaimehp@gmail.com>
2023-03-08 12:14:24 -0600

    replaced team trailer link

---
+++ _site/README 2.md
@@ -0,0 +1,15 @@
+# Hack the Threat 2023
+
+
+##[GitPages: https://hackhpc.github.io/HacktheThreat23/](https://hackhpc.github.io/HacktheThreat23/)
+
+
+## Description:
+The Texas Advanced Computing Center (TACC) is partnering with Simon Fraser University (SFU) to offer an engaging virtual Hackathon from March 2 - 6, 2023. During this competition, participants will be presented with data challenges and problems within the scope of cybersecurity, environmental, and social threats. Participants will be able to choose their challenge of interest and form teams, working collaboratively to explore and develop solutions within a given time frame. Pre-event virtual training sessions will provide information on computational resources that will be made available to the participants. Data may be provided by team mentors, TACC or SFU researchers, or taken from public repositories. Participants will use Jupyter notebooks, Python, and other programming languages to address the hackathon goals. Teams will present their findings to a panel of judges and industry experts for final evaluation. Prizes will be awarded to the winning teams.
+
+## Participation:
+We would like to invite Students/Hackers from SFU, UBC, or UT Austin who are enrolled in undergraduate or graduate programs with an interest in data processes and analysis. This hackathon offers a unique opportunity for students to develop analytical and collaboration skills which are essential in achieving career goals. 
+
+
+## [Click Here to Register](https://www.eventbrite.ca/e/2023-hack-the-threat-hackathon-tickets-519286298297)
+<a href="https://www.eventbrite.ca/e/2023-hack-the-threat-hackathon-tickets-519286298297"><img src="assets/img/HacktheThreat.png"></a>
---
+++ _site/assets/js/calendar 2.js
@@ -0,0 +1,108 @@
+// Ref: https://fullcalendar.io/
+
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var initialTimeZone = 'local';
+    var calendarlocalTZ = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    
+
+    timeZoneEl.insertAdjacentText('beforeend', calendarlocalTZ);
+   
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      aspectRatio: 1,
+      initialView: 'listYear', //'listMonth', //'dayGridWeek',
+      timeZone: initialTimeZone,
+      eventTimeFormat: { hour: 'numeric', minute: '2-digit' },
+      initialDate: '2023-03-01',
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title' ,
+        right: 'listYear,dayGridMonth,timeGridWeek,timeGridDay'
+      },
+
+      views: {
+        dayGridMonth: { buttonText: 'month' },
+        timeGridDay: { buttonText: 'day' },
+        timeGridWeek: { buttonText: 'week' },
+        listMonth: { buttonText: 'list month' },
+        listYear: { buttonText: 'year'}
+      },
+      
+     
+
+      events: [
+        {
+          title: 'Mentor Training',
+          start: '2023-02-28T11:00:00-06:00',
+          end: '2023-02-28T12:00:00-06:00',
+          backgroundColor: 'orange',
+          url: './resources#Mentor%20Training',
+        },
+        {
+          title: 'All Participants Orientation',
+          start: '2023-02-23T11:00:00-06:00',
+          end: '2023-02-23T12:00:00-06:00',
+          backgroundColor: 'orange',
+          url: './resources#All%20Participants%20Orientation'
+        },
+        {
+          title: 'Hackathon Kick-Off!',
+          start: '2023-03-02T18:00:00-06:00',
+          end: '2023-03-02T20:00:00-06:00',
+          backgroundColor: 'green',
+          url: './resources#Hackathon%20Kick-Off!'
+        },
+        {
+          title: 'Friday Morning Checkin',
+          start: '2023-03-03T11:00:00-06:00',
+          end: '2023-03-03T12:00:00-06:00',
+          //backgroundColor: 'orange'
+          url: './resources#Friday%20Morning%20Checkin'
+        },
+        {
+          title: 'Friday Afternoon Checkin',
+          start: '2023-03-03T18:00:00-06:00',
+          end: '2023-03-03T19:00:00-06:00',
+          //backgroundColor: 'orange'
+          url: './resources#Friday%20Afternoon%20Checkin'
+        },
+        {
+          title: 'Saturday Morning Checkin',
+          start: '2023-03-04T11:00:00-06:00',
+          end: '2023-03-04T12:00:00-06:00',
+          //backgroundColor: 'orange'
+          url: './resources#Saturday%20Morning%20Checkin'
+        },
+        {
+          title: 'Saturday Afternoon Checkin',
+          start: '2023-03-04T18:00:00-06:00',
+          end: '2023-03-04T19:00:00-06:00',
+          //backgroundColor: 'orange'
+          url: './resources#Saturday%20Afternoon%20Checkin'
+        },
+        {
+          title: 'Sunday Morning Checkin',
+          start: '2023-03-05T11:00:00-06:00',
+          end: '2023-03-05T12:00:00-06:00',
+          //backgroundColor: 'orange'
+          url: './resources#Sunday%20Morning%20Checkin'
+        },
+        {
+          title: 'Monday Morning Checkin',
+          start: '2023-03-06T11:00:00-06:00',
+          end: '2023-03-06T12:00:00-06:00',
+          //backgroundColor: 'orange'
+          url: './resources#Monday%20Morning%20Checkin'
+        },
+        {
+          title: 'Final Presentations',
+          start: '2023-03-06T18:00:00-06:00',
+          end: '2023-03-06T20:00:00-06:00',
+          backgroundColor: 'red',
+          url: './resources#Final%20Presentations'
+        },
+      ]
+    });
+    
+    calendar.render();
+  });
